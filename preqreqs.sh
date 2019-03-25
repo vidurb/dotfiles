afe0f9b30523044af0d9afad69854e768cefb7cd
Vidur Butalia <vidurbutalia@gmail.com>
2019-03-25 23:31:22 +0530

    Add script to get some prereqs automatically.wq

---
+++ preqreqs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Add vim-plug
+cd 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Cloned vim-plug!"
+# Add oh-my-tmux
+git clone https://github.com/samoshkin/tmux-config.git
+echo "Cloned oh-my-tmux"
+ln -s -f .tmux/.tmux.conf
+echo "Linked oh-my-tmux"
+# Add tmux-nord theme
+git clone https://github.com/arcticicestudio/nord-tmux ~/.tmux/themes/nord-tmux
