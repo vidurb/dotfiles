6e09d306a45d86d62dd0aa13560f200818f1eec0
Vidur Butalia <vidurbutalia@gmail.com>
2019-06-05 23:59:59 +0530

    Only trigger vim-plug install when existing install is not detected

--- install.sh
+++ install.sh
@@ -3,28 +3,36 @@
 for directory in */;
 do 
     if [[ ${directory%/} == nvim ]]; then
-        read -r -p "Install vim-plug for nvim?" response
-        case "$response" in
-            [yY][eE][sS]|[yY]) 
-                curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-                echo "vim-plug installed in ~/.local/share/nvim"
+        if [[ -e  ~/.local/share/nvim/site/autoload/plug.vim ]]; then
+            echo "vim-plug already installed"
+        else
+            read -r -p "Install vim-plug for nvim?" response
+            case "$response" in
+                [yY][eE][sS]|[yY]) 
+                    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+                    echo "vim-plug installed in ~/.local/share/nvim"
+                    ;;
+                *)
+                    echo "vim-plug not installed"
                 ;;
-            *)
-                echo "vim-plug not installed"
-            ;;
-        esac
+            esac
+        fi
     fi
     if [[ ${directory%/} == vim ]]; then
-        read -r -p "Install vim-plug for vim?" response
-        case "$response" in
-            [yY][eE][sS]|[yY]) 
-                curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-                echo "vim-plug installed in ~/.vim"
+        if [[ -e  ~/.local/share/nvim/site/autoload/plug.vim ]]; then
+            echo "vim-plug already installed"
+        else
+            read -r -p "Install vim-plug for vim?" response
+            case "$response" in
+                [yY][eE][sS]|[yY]) 
+                    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+                    echo "vim-plug installed in ~/.vim"
+                    ;;
+                *)
+                    echo "vim-plug not installed"
                 ;;
-            *)
-                echo "vim-plug not installed"
-            ;;
-        esac
+            esac
+        fi
     fi
     read -r -p "Symlink the contents of ${directory%/} into $HOME?" response
     case "$response" in
