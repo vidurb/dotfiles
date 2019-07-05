146569584a8647b5ce5660195f5e0832c26d978b
Vidur Butalia <vidurbutalia@gmail.com>
2019-07-05 11:12:55 +0530

    Remove aliases for google-drive-ocamlfuse

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,5 @@
 set fish_greeting
 
-alias mount-personal="google-drive-ocamlfuse -label personal /home/vidur/GoogleDrive/personal"
-alias mount-store="google-drive-ocamlfuse -label store /home/vidur/GoogleDrive/store"
-alias mount-miranj="google-drive-ocamlfuse -label miranj /home/vidur/GoogleDrive/miranj"
 alias tmux="tmux -u -2"
 
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
