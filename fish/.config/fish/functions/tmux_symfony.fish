2e9d546cd2e118b5adc8c60d139b3b19228bef67
Vidur Butalia <vidur.butalia@getastra.com>
2019-07-11 23:32:00 +0530

    Remove unused vim window and add case for astra-dash

--- fish/.config/fish/functions/tmux_symfony.fish
+++ fish/.config/fish/functions/tmux_symfony.fish
@@ -2,10 +2,14 @@
 function tmux_symfony --description="Creates/attaches to tmux session for symfony, arg is directory"
     set -l project_path $argv[1]
     set -l session_name (string split -r -m1 '/' $project_path)[2]
+    if test $project_path = '/home/vidur/Sources/astra-dash'
+        set server_command 'symfony server:start --port=8002 --allow-http'
+    else 
+        set server_command 'symfony server:start'
+    end
     tmux \
     	new-session -AD -t $session_name -c $project_path \; \
     	rename-window symfony \; \
-    	split-window -vb -p 25 'symfony server:start' \; \
-    	new-window -d -c $project_path -n vim 'vim src' \; \
+        split-window -vb -p 25 $server_command \; \
     	select-pane -D
 end
