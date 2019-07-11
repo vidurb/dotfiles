1b692abe0dd12cfaf3c3a5ad3c3bd78bd1ccdd40
Vidur Butalia <vidur.butalia@getastra.com>
2019-07-12 00:09:24 +0530

    Add symfony proxy server initiation to tmux_symfony function

--- fish/.config/fish/functions/tmux_symfony.fish
+++ fish/.config/fish/functions/tmux_symfony.fish
@@ -3,9 +3,9 @@ function tmux_symfony --description="Creates/attaches to tmux session for symfon
     set -l project_path $argv[1]
     set -l session_name (string split -r -m1 '/' $project_path)[2]
     if test $project_path = '/home/vidur/Sources/astra-dash'
-        set server_command 'symfony server:start --port=8002 --allow-http'
+        set server_command 'symfony proxy:start && symfony server:start --port=8002 --allow-http'
     else 
-        set server_command 'symfony server:start'
+        set server_command 'symfony proxy:start && symfony server:start'
     end
     tmux \
     	new-session -AD -t $session_name -c $project_path \; \
