6102033e75977d096166794eadd745550be1a505
Vidur Butalia <vidur.butalia@getastra.com>
2019-07-12 11:43:06 +0530

    Got tmux_symfony working && usable on other systems

--- fish/.config/fish/functions/tmux_symfony.fish
+++ fish/.config/fish/functions/tmux_symfony.fish
@@ -2,18 +2,15 @@
 function tmux_symfony --description="Creates/attaches to tmux session for symfony, arg is directory"
     set -l project_path $argv[1]
     set -l project_name (string split -r -m1 '/' $project_path)[2]
-    if $TMUX
-        echo 'TMUX!'
-    end
     if string match --quiet --ignore-case --regex "(astra-dash)" $project_path
         set server_command 'symfony proxy:start && symfony server:start --port=8002 --allow-http'
     else 
         set server_command 'symfony proxy:start && symfony server:start'
     end
-    if -n $TMUX
+    if test -n $TMUX
         tmux \
-    	    new-window -n $project_name \; \
-            split-window -vb -p 25 $server_command \; \
+    	    new-window -n $project_name "cd $project_path && $SHELL" \; \
+            split-window -vb -p 25 "cd $project_path && $server_command" \; \
     	    select-pane -D
     else
         tmux \
