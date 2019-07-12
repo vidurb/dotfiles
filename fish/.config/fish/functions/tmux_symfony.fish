60f6f46dfa3633596cd0598d6a480e2bd3a3b926
Vidur Butalia <vidur.butalia@getastra.com>
2019-07-12 11:10:57 +0530

    Modify function to open new window if tmux is already running

--- fish/.config/fish/functions/tmux_symfony.fish
+++ fish/.config/fish/functions/tmux_symfony.fish
@@ -1,15 +1,25 @@
 # Create or attach to Symfony tmux session
 function tmux_symfony --description="Creates/attaches to tmux session for symfony, arg is directory"
     set -l project_path $argv[1]
-    set -l session_name (string split -r -m1 '/' $project_path)[2]
+    set -l project_name (string split -r -m1 '/' $project_path)[2]
+    if $TMUX
+        echo 'TMUX!'
+    end
     if string match --quiet --ignore-case --regex "(astra-dash)" $project_path
         set server_command 'symfony proxy:start && symfony server:start --port=8002 --allow-http'
     else 
         set server_command 'symfony proxy:start && symfony server:start'
     end
-    tmux \
-    	new-session -AD -t $session_name -c $project_path \; \
-    	rename-window symfony \; \
-        split-window -vb -p 25 $server_command \; \
-    	select-pane -D
+    if -n $TMUX
+        tmux \
+    	    new-window -n $project_name \; \
+            split-window -vb -p 25 $server_command \; \
+    	    select-pane -D
+    else
+        tmux \
+    	    new-session -AD -t symfony -c $project_path \; \
+    	    rename-window $project_name \; \
+            split-window -vb -p 25 $server_command \; \
+    	    select-pane -D
+    end
 end
