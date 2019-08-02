d4ae1e2c31d19078230ed2248647ecb88db0f2fa
Vidur Butalia <vidurbutalia@gmail.com>
2019-08-02 13:54:05 +0530

    Comment config.fish and set EDITOR to vim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,8 @@
+# Remove shell greeting
 set fish_greeting
-
+# force tmux to be 256color and unicode compatible
 alias tmux="tmux -u -2"
-
+# Add to path from .profile
 bass source ~/.profile
+# Set default editor
+set -gx EDITOR vim
