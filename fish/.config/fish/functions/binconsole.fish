6afe766fddd7eeed71abc2b61c08d69b4e335656
Vidur Butalia <vidurbutalia@gmail.com>
2019-08-04 20:04:27 +0530

    Remove bin/console wrapper, I discovered symfony cli includes one

--- fish/.config/fish/functions/binconsole.fish
+++
@@ -1,8 +0,0 @@
-# Shortcut to bin/console for symfony projects
-function binconsole --description "Shortcut for php bin/console"
-	if test -e bin/console
-		php bin/console $argv
-	else
-		echo "bin/console not found"
-	end
-end
