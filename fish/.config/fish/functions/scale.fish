065f91b80493df1ae8f955b6583dc43e0bc66685
Vidur Butalia <vidurbutalia@gmail.com>
2019-11-01 12:12:54 +0530

    Add second monitor to scale function

--- fish/.config/fish/functions/scale.fish
+++ fish/.config/fish/functions/scale.fish
@@ -2,10 +2,10 @@
 function scale-right --description "-d for double dashes in output names, -n for no dashes"
 	argparse --name=scale-right --max-args=1 'd/doubledash' 'n/nodash' -- $argv
 	if set -q _flag_d
-		xrandr --output eDP-1-1 --auto --scale 1x1 --output HDMI-1-1 --auto --scale 2x2
+		xrandr --output eDP-1-1 --auto --scale 1x1 --output HDMI-1-1 --auto --scale 2x2 --output DP-1-1 --auto --scale 2x2
 	else if set -q _flag_n
-		xrandr --output eDP1 --auto --scale 1x1 --output HDMI1 --auto --scale 2x2
+		xrandr --output eDP1 --auto --scale 1x1 --output HDMI1 --auto --scale 2x2 --output DP1 --auto --scale 2x2
 	else
-		xrandr --output eDP-1 --auto --scale 1x1 --output HDMI-1 --auto --scale 2x2
+		xrandr --output eDP-1 --auto --scale 1x1 --output HDMI-1 --auto --scale 2x2 --output DP-1 --auto --scale 2x2
 	end
 end
