dbc3551ee9c3b3ca3ce41ec6d741d4e0e3f0befa
Vidur Butalia <vidurbutalia@gmail.com>
2020-02-24 17:58:52 +0530

    Fix typo in install_rust_utils.sh

--- install_rust_utils.sh
+++ install_rust_utils.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-
+echo "Installing Rustlang, Cargo package manager and C/C++/Rust build tools"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt install -y build-essentials libgit2 cmake llvm
+sudo apt install -y build-essential libgit2 cmake llvm
 cargo install exa
 cargo install bat
 cargo install starship
