740c3f92855054cf6a1db519708645dba4615b2d
Vidur Butalia <vidurbutalia@gmail.com>
2020-07-22 17:12:49 +0530

    Improve fish performance hopefully
    Moved almost everything into the block predicated on being called in an interactive session

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,3 @@
-# Remove shell greeting
-set fish_greeting
-# force tmux to be 256color and unicode compatible
-alias tmux="tmux -u -2"
-alias mosh="mosh -p 62713"
-alias clip='xsel --clipboard --input'
-
 # Add extra directories to path from bindirs.txt
 cat "$__fish_config_dir/bindirs.txt" | read --list -d : __extra_path_dirs
 for dir in __extra_path_dirs
@@ -30,9 +23,6 @@ if status --is-interactive
     abbr --add --global gf git fetch
     abbr --add --global gco git checkout
     abbr --add --global gfap git fetch --all --prune
-    abbr --add --global merge-master lab mr create origin master -a vidur -m
-    abbr --add --global merge-future lab mr create origin future -a vidur -m
-    abbr --add --global merge-develop lab mr create origin develop -a vidur -m
     abbr --add --global sc symfony console
     abbr --add --global d-c docker-compose
     abbr --add --global sbc bin/console
@@ -60,35 +50,43 @@ if status --is-interactive
             alias code=code-insiders
         end
     end
+
+    # Remove shell greeting
+    set fish_greeting
+
+    # force tmux to be 256color and unicode compatible
+    alias tmux="tmux -u -2"
+    alias mosh="mosh -p 62713"
+    alias clip='xsel --clipboard --input'
+
+    # Set nord theme
+    set -U fish_color_normal normal
+    set -U fish_color_command 81a1c1
+    set -U fish_color_quote a3be8c
+    set -U fish_color_redirection b48ead
+    set -U fish_color_end 88c0d0
+    set -U fish_color_error ebcb8b
+    set -U fish_color_param eceff4
+    set -U fish_color_selection white --bold --background=brblack
+    set -U fish_color_search_match bryellow --background=brblack
+    set -U fish_color_history_current --bold
+    set -U fish_color_operator 00a6b2
+    set -U fish_color_escape 00a6b2
+    set -U fish_color_cwd green
+    set -U fish_color_cwd_root red
+    set -U fish_color_valid_path --underline
+    set -U fish_color_autosuggestion 4c566a
+    set -U fish_color_user brgreen
+    set -U fish_color_host normal
+    set -U fish_color_cancel -r
+    set -U fish_pager_color_completion normal
+    set -U fish_pager_color_description B3A06D yellow
+    set -U fish_pager_color_prefix white --bold --underline
+    set -U fish_pager_color_progress brwhite --background=cyan
+
+    # Make shell prompt rocket
+    starship init fish | source
 end
 
-# Set nord theme
-set -U fish_color_normal normal
-set -U fish_color_command 81a1c1
-set -U fish_color_quote a3be8c
-set -U fish_color_redirection b48ead
-set -U fish_color_end 88c0d0
-set -U fish_color_error ebcb8b
-set -U fish_color_param eceff4
-set -U fish_color_selection white --bold --background=brblack
-set -U fish_color_search_match bryellow --background=brblack
-set -U fish_color_history_current --bold
-set -U fish_color_operator 00a6b2
-set -U fish_color_escape 00a6b2
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion 4c566a
-set -U fish_color_user brgreen
-set -U fish_color_host normal
-set -U fish_color_cancel -r
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description B3A06D yellow
-set -U fish_pager_color_prefix white --bold --underline
-set -U fish_pager_color_progress brwhite --background=cyan
 
-export ANSIBLE_COW_SELECTION=random
-export ANSIBLE_NOCOWS=1
-export LESSCHARSET=utf-8
-# Starship!
-starship init fish | source
+
