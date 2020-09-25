942363063bc57e13d361178770fd54d9ec305b12
Vidur Butalia <vidurbutalia@gmail.com>
2020-09-25 15:32:11 +0530

    Delete unused fish functions

--- dot_config/fish/functions/generate_vhost.fish
+++
@@ -1,8 +0,0 @@
-function generate_vhost
-    mkdir /home/vidur/Sites/$argv[1]
-    sudo ln -s "/home/vidur/Sites/$argv[1]" "/var/www/www.$argv[1].test"
-    sudo ln -s "/home/vidur/Sites/$argv[1]" "/var/www/$argv[1].test"
-    sudo ln -s "/home/vidur/Sites/$argv[1]" "/var/www/www.$argv[1].vidur"
-    sudo ln -s "/home/vidur/Sites/$argv[1]" "/var/www/$argv[1].vidur"
-    sudo chown -R vidur:www-data /home/vidur/Sites/$argv[1]
-end
