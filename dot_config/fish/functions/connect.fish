16b183ce969436fc27f653e623ef9c78ec24141c
Vidur Butalia <vidurbutalia@gmail.com>
2021-03-12 00:59:43 +0530

    Update connect.fish

--- dot_config/fish/functions/connect.fish
+++ dot_config/fish/functions/connect.fish
@@ -1,6 +1,6 @@
 function connect -d "SSH/Mosh to standard tmux session"
   if type -q "mosh"
-    mosh $argv -t tmux -u -2 new -A -s vidur
+    mosh --ssh="$argv -t tmux -u -2 new -A -s vidur"
   else
     ssh $argv -t tmux -u -2 new -A -s vidur
   end
