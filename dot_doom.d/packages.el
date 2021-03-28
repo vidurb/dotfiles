2217c4a9173b81ab5405fe9a97d7f37a53978a51
Vidur Butalia <vidurbutalia@gmail.com>
2021-03-28 09:58:17 +0530

    Update doom config

--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -1,7 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! nord-theme)
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
