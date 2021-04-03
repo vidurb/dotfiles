a7d9666b22bb2ae1eed7fa3693a375f60e46eb92
Vidur Butalia <vidurbutalia@gmail.com>
2021-04-03 19:40:43 +0530

    Delete switch-kubeconfig.fish

--- dot_config/fish/functions/switch-kubeconfig.fish
+++
@@ -1,4 +0,0 @@
-function switch-kubeconfig --description="Switch kubeconfig by name"
-  argparse --name "switch-kubeconfig" --max-args=0 'c/config=' -- $argv
-  ln -sF "$HOME/.kube/$_flag_c" ~/.kube/config
-end
