e92217a537329746eaaa1611bc276a1cbdb49c3b
Vidur Butalia <vidurbutalia@gmail.com>
2021-04-03 11:08:07 +0530

    Fix switch-kubeconfig function

--- dot_config/fish/functions/switch-kubeconfig.fish
+++ dot_config/fish/functions/switch-kubeconfig.fish
@@ -1,4 +1,4 @@
 function switch-kubeconfig --description="Switch kubeconfig by name"
-  argparse --name "switch-kubeconfig" --min-args=1 --max-args=1 'c/config=' -- $argv
-  ln -sF "~/.kube/$_flag_c" ~/.kube/config
+  argparse --name "switch-kubeconfig" --max-args=0 'c/config=' -- $argv
+  ln -sF "$HOME/.kube/$_flag_c" ~/.kube/config
 end
