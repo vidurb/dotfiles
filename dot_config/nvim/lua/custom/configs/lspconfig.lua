b796ffb27d9df492ff961a057b2576aabac918d0
Vidur Butalia <vidurbutalia@gmail.com>
2023-07-17 00:17:56 +0530

    Configure language servers for nvim

--- dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -2,11 +2,12 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd"}
+local servers = {"html", "cssls", "sqlls", "stylelint_lsp", "terraformls", "prismals", "tailwindcss", "jsonls",
+                 "eslint", "helm_ls", "dockerls", "docker_compose_language_service", "elixirls", "tsserver"}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities
+    }
 end
--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -1,90 +1,33 @@
-local plugins = {
-  {
+local plugins = {{
     "neovim/nvim-lspconfig",
-     dependencies = {
-       "jose-elias-alvarez/null-ls.nvim",
-       config = function()
-         require "custom.configs.null-ls"
-       end,
-     },
-     config = function()
+    dependencies = {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+            require "custom.configs.null-ls"
+        end
+    },
+    config = function()
         require "plugins.configs.lspconfig"
         require "custom.configs.lspconfig"
-     end,
-  },  
-  {
+    end
+}, {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-        "json",
-        "bash",
-        "fish",
-        "dart",
-        "dockerfile",
-        "elixir",
-        "git_config",
-        "gitignore",
-        "gitcommit",
-        "go",
-        "hcl",
-        "jsonnet",
-        "nix",
-        "passwd",
-        "php",
-        "proto",
-        "python",
-        "rust",
-        "sql",
-        "scss",
-        "terraform",
-        "toml",
-        "twig",
-        "yaml"
-      },
-    },
-  },
-  {
+        ensure_installed = {"vim", "lua", "html", "css", "javascript", "typescript", "tsx", "json", "bash", "fish",
+                            "dart", "dockerfile", "elixir", "git_config", "gitignore", "gitcommit", "go", "hcl",
+                            "jsonnet", "nix", "passwd", "php", "proto", "python", "rust", "sql", "scss", "terraform",
+                            "toml", "twig", "yaml"}
+    }
+}, {
     "williamboman/mason.nvim",
     opts = {
-       ensure_installed = {
-         "lua-language-server",
-         "actionlint",
-         "commitlint",
-         "css-lsp",
-         "docker-compose-language-service",
-         "dockerfile-language-server",
-         "elixir-ls",
-         "eslint-lsp",
-         "firefox-debug-adapter",
-         "gh",
-         "hadolint",
-         "html-lsp",
-         "helm-ls",
-          "json-lsp",
-          "markdown-toc",
-          "markdownlint",
-          "misspell",
-          "prettierd",
-          "prisma-language-server",
-          "ruff",
-          "shellcheck",
-          "sqlfmt",
-          "sqlls",
-          "stylelint",
-          "stylelint-lsp",
-          "tailwindcss-language-server",
-          "terraform-ls",
-          "yamllint"
-       },
-     },
-   } 
-}
+        ensure_installed = {"lua-language-server", "actionlint", "commitlint", "css-lsp",
+                            "docker-compose-language-service", "dockerfile-language-server", "elixir-ls", "eslint-lsp",
+                            "firefox-debug-adapter", "gh", "hadolint", "html-lsp", "helm-ls", "json-lsp",
+                            "markdown-toc", "markdownlint", "misspell", "prettierd", "prisma-language-server", "ruff",
+                            "shellcheck", "sqlfmt", "sqlls", "stylelint", "stylelint-lsp",
+                            "tailwindcss-language-server", "terraform-ls", "typescript-language-server", "yamllint"}
+    }
+}}
 
 return plugins
