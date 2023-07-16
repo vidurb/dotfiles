61c78708b3ac2b7e64d781937b3794e975581ee2
Vidur Butalia <vidurbutalia@gmail.com>
2023-07-16 18:28:13 +0530

    Configure wezterm to replace tmux

---
+++ dot_wezterm.lua
@@ -0,0 +1,83 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = 'nord'
+
+config.font_size = 13.0
+
+config.check_for_updates = false
+
+config.inactive_pane_hsb = { saturation = 0.9, brightness = 0.8 }
+
+config.launch_menu = {}
+
+config.leader = { key="a", mods="CTRL" }
+
+config.disable_default_key_bindings = true
+
+config.keys = {
+    -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+    { key = "a", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
+    { key = "-", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+    { key = "\\",mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+    { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
+    { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+    { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
+    { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
+    { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
+    { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
+    { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+    { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+    { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+    { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+    { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
+    { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
+    { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
+    { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
+    { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
+    { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
+    { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
+    { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
+    { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
+    { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
+    { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
+
+    { key = "n", mods="SHIFT|CTRL",     action="ToggleFullScreen" },
+    { key="v",   mods="SHIFT|CTRL",     action=wezterm.action.PasteFrom 'Clipboard'},
+    { key="c",   mods="SHIFT|CTRL",     action=wezterm.action.CopyTo 'Clipboard'},
+}
+
+config.set_environment_variables = {}
+
+config.ssh_domains = {
+    {
+        name = 'centerpoint',
+        remote_address = 'centerpoint.beetabus.vidur.xyz',
+        username = 'vidur'
+    },
+    {
+        name = 'matrix',
+        remote_address = 'matrix.vidur.xyz',
+        username = 'ubuntu'
+    }
+}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.front_end = "Software" -- OpenGL doesn't work quite well with RDP.
+    config.term = "" -- Set to empty so FZF works on windows
+    config.default_prog = { "powershell.exe", "-NoLogo" }
+    table.insert(config.launch_menu, { label = "cmd", args = {"cmd.exe"} })
+elseif wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple == "aarch64-apple-darwin" then
+    config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+    table.insert(config.launch_menu, { label = "zsh", args = {"zsh", "-l"} })
+else
+    config.default_prog = { "fish", "-l" }
+    table.insert(config.launch_menu, { label = "bash", args = {"bash", "-l"} })
+end
+
+return config
